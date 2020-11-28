be3f9b08ae35192628a682117468e30e7abd8e03
Ross Boe <Ross.Boe@trueit.com>
2020-11-28 07:01:03 -0600

    add console app

---
+++ src/Cds-App-Registration/Cds-App-Registration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cds_App_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
